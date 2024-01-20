e6c4ea6da17116f43dc19f457ba65bf415392f27
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-21 04:16:58 +0530

    Python

--- 1.basic/count_digits.py
+++ 1.basic/count_digits.py
@@ -1,24 +1,5 @@
 //  Q) Given an integer N, write a program to count the number of digits in N.
 
-//  C 
-#include<stdio.h>
-int No_digits(int num){
-    int t=num;
-    int s=0;
-    while(t>0){
-        t=t/10;
-        s++;
-    }
-    return s;
-}
-int main(){
-    int num;
-    printf("Enter the number");
-    scanf("%d",&num);
-    int c=No_digits(num);
-    printf("Number of digits are %d ",c);
-}
-
 # python
 
 def No_digits(num):
