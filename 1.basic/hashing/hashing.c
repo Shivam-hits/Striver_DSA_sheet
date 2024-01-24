2bfc0032a99a1c67c77a26da0bd21a4bbb3bc878
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-25 03:09:57 +0530

    hashing in c

--- 1.basic/hashing/hashing.c
+++ 1.basic/hashing/hashing.c
@@ -1,24 +1,33 @@
 // To find the frequedcy of numbers of an array
 #include<stdio.h>
+#include<string.h>
 
 int main(){
-    int n,c;
+    int n;
     int key;
     printf("Enter the length of array: ");
     scanf("%d",&n);
     int arr[n];
-    int hash[n+1];
+    int hash[n+1];// = {0} --> this happen in c++ and to do this we have to use memset
+
+    // ptr ==> Starting address of memory to be filled
+    // x   ==> Value to be filled
+    // n   ==> Number of bytes to be filled starting 
+    //         from ptr to be filled
+    //void *memset(void *ptr, int x, size_t n);
+    // to use memset we have to use string.h {#include<string.h>} preprocessor 
+    memset(hash,0,sizeof(hash));
     
     for(int i=0;i<n;i++){
         printf("Enter %d element: ",i);
         scanf("%d",&arr[i]);
     }
 
-    printf("Enter key element: ");
-    scanf("%d",&key);
-
     for(int i=0;i<n;i++){
         hash[arr[i]] += 1;
     }
-    printf("Number of occurance of %d is %d",key,hash[key]);
+    printf("Enter key element: ");
+    scanf("%d",&key);
+
+    printf("output is %d",hash[key]);
 }
--- 1.basic/hashing/hashing.cpp
+++ 1.basic/hashing/hashing.cpp
@@ -7,21 +7,16 @@ int main(){
     int key;    
     cout<<"Enter the length of array: ";
     cin>>n;
-
     int arr[n];
     int hash[13]={0};
-    
     for(int i=0;i<n;i++){
         cout << "Enter the "<< i <<" element ";
         cin >> arr[i];
     }
-    
     for(int i=0;i<n;i++){
         hash[arr[i]] += 1;
     }
-
     cout<<"Enter the element: ";
     cin>>key;
     cout<<"output "<<hash[key];
-
 }
