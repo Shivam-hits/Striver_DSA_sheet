29c8df7aecc13a6c09c6aafe4cb6648756346c99
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-31 01:25:46 +0530

    Union

---
+++ 3.array/Easy/Linear_search/ans.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class ans{
+    public static void main()
+    {
+        Scanner sc=new Scanner(system.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+    }
+}
---
+++ 3.array/Easy/Union_of_array/union.java
@@ -0,0 +1,13 @@
+// union of 2 array
+import java.util.*;
+
+class union{
+    public static void main(){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int arr1[]=new Int[n];
+        int arr2[]=new Int[m];
+
+    }
+}
